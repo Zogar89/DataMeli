9f718ea5bc529c8ef6cc69507bb4f79b39fa47dd
Zogar89 <gabrielalejandrogarcia@gmail.com>
2022-09-14 04:10:40 -0300

    Comienzo de Q2

---
+++ Q2.sql
@@ -0,0 +1,3 @@
+# Este tema no lo conocía así que seguí este video https://www.youtube.com/watch?v=GJ3M3daJA_E
+# Para la configuración del servidor al usar una instancia administrada de AWS RDS no cuento con privilegios de superuser, entonces seguí esta guia para aplicar un nuevo grupo de parámetros con event_scheduler ON https://stackoverflow.com/questions/14940910/creating-mysql-events-in-amazon-rds 
+
