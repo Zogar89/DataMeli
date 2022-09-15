7320d7915bb509354d4fe4fda6f1ad43f915771e
Zogar89 <gabrielalejandrogarcia@gmail.com>
2022-09-14 22:46:52 -0300

    Corrección de errores pendientes
    Primary key
    Orden de fechas

--- Q1.sql
+++ Q1.sql
@@ -5,5 +5,5 @@ SELECT 	COUNTRY,
 FROM transactions
 JOIN sellers
   ON  sellers.CUST_ID = transactions.CUST_ID
-WHERE DATEDIFF(TRANSACTION_DATE, CURDATE()) between 0 and 30 #No olvidar de cambiar el orden y la fecha de creación de los ejemplos
+WHERE DATEDIFF(TRANSACTION_DATE, CURDATE()) between -30 and 0 #No olvidar de cambiar el orden y la fecha de creación de los ejemplos
 GROUP BY COUNTRY, DATE_YM, sellers.TIPO_CLIENTE;
